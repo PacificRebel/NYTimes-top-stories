d10094daf0f22b3c7d592d2f708413ea9a64a2be
Heli Sivunen <heli@mooncat.tv>
2020-10-13 17:42:27 +0100

    Downloaded Google Fonts for gatsby, implemented on headlines but not header.

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,11 +2,20 @@ require("dotenv").config()
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `New York Times top stories`,
+    description: `Calls New York Times API for access to their top stories.`,
     author: `@gatsbyjs`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `domine`,
+          `serif`,
+        ],
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-import TopStories from "../components/TopStories
+import TopStories from "../components/TopStories"
 
 import { Column } from 'simple-flexbox';
 
@@ -12,11 +12,9 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
 
-    <h1>New York Times top stories</h1>
-
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
         <TopStories />
-      <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
