b0107c9ff6207b8fc225fa8222d168b08d78c44a
Heli Sivunen <heli@mooncat.tv>
2020-11-16 12:20:06 +0000

    Clean up ununsed imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,7 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 import TopStories from "../components/TopStories"
-
-import { Column } from 'simple-flexbox';
 
 const IndexPage = () => (
   <Layout>
